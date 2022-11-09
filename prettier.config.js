db8e416ad23d05db454bd5871ff8094f2093f44f
Alexander Schnebel <alexander@schnebel.dev>
2022-11-09 18:28:57 +0100

    deps: add tailwindcss-prettier-config

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  singleQuote: true,
+  printWidth: 140,
+  semi: false,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  plugins: [require('prettier-plugin-tailwindcss')],
+}
