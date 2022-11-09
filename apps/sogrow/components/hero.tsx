f4fad35d53035625a46ed353a9fd24ba30f044ce
Alexander Schnebel <alexander@schnebel.dev>
2022-11-09 19:12:15 +0100

    fix: remove rect pattern for now

--- apps/sogrow/components/hero.tsx
+++ apps/sogrow/components/hero.tsx
@@ -1,39 +1,6 @@
 export function Hero() {
   return (
-    <div className="relative overflow-hidden">
-      <div className="hidden md:absolute md:inset-y-0 md:block md:h-full md:w-full" aria-hidden="true">
-        <div className="relative mx-auto h-full max-w-7xl">
-          <svg
-            className="absolute right-full translate-y-1/4 translate-x-1/4 transform lg:translate-x-1/2"
-            width={404}
-            height={784}
-            fill="none"
-            viewBox="0 0 404 784"
-          >
-            <defs>
-              <pattern id="f210dbf6-a58d-4871-961e-36d5016a0f49" x={0} y={0} width={20} height={20} patternUnits="userSpaceOnUse">
-                <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
-              </pattern>
-            </defs>
-            <rect width={404} height={564} fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
-          </svg>
-          <svg
-            className="absolute left-full -translate-y-3/4 -translate-x-1/4 transform md:-translate-y-1/2 lg:-translate-x-1/2"
-            width={404}
-            height={784}
-            fill="none"
-            viewBox="0 0 404 784"
-          >
-            <defs>
-              <pattern id="5d0dd344-b041-4d26-bec4-8d33ea57ec9b" x={0} y={0} width={20} height={20} patternUnits="userSpaceOnUse">
-                <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
-              </pattern>
-            </defs>
-            <rect width={404} height={784} fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)" />
-          </svg>
-        </div>
-      </div>
-
+    <>
       <section className="mx-auto mt-24 flex max-w-7xl flex-col items-center px-4 text-center sm:mt-24">
         <p className="text-4xl font-bold text-gray-900 sm:text-5xl md:text-7xl">
           <span className="block">Never</span>
@@ -55,7 +22,7 @@ export function Hero() {
           <h2 className="text-2xl font-medium text-gray-900 sm:text-4xl md:text-5xl">How we help you grow on Twitter</h2>
         </div>
       </section>
-    </div>
+    </>
   )
 }
 
