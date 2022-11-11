2db2537aedbf26ddd2cb82b8b886951f1b9d617b
Alexander Schnebel <alexander@schnebel.dev>
2022-11-11 19:00:10 +0100

    feat: Link user back to Home on Logo click

--- apps/sogrow/components/logo.tsx
+++ apps/sogrow/components/logo.tsx
@@ -1,15 +1,16 @@
 import { Bebas_Neue } from '@next/font/google';
+import Link from 'next/link';
 
 const bebasNeue = Bebas_Neue({ weight: '400', subsets: ['latin'] })
 
 export function Logo() {
   return (
-    <span className={bebasNeue.className}>
+    <Link href="/" className={bebasNeue.className}>
       <p className="text-3xl">
       <span className="text-gray-500 text-bold">So</span>
       <span className="text-gray-900">grow</span>
     </p>
-    </span>
+    </Link>
   )
 }
 
