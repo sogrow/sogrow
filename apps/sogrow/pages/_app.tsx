b7725fbf4cfe4269a1168fb9956f6302a694de27
Alexander Schnebel <alexander@schnebel.dev>
2022-11-12 18:44:33 +0100

    deps: init react query client

--- apps/sogrow/pages/_app.tsx
+++ apps/sogrow/pages/_app.tsx
@@ -2,17 +2,24 @@ import { AppProps } from 'next/app'
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
 import './styles.css'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import React from 'react'
+import { ReactQueryDevtools } from 'react-query/devtools';
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({ subsets: ['latin'] })
 
 function CustomApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = React.useState(() => new QueryClient())
   return (
     <>
       <Head>
         <title>Sogrow • Grow your audience with confidence</title>
       </Head>
       <main className={inter.className}>
-        <Component {...pageProps} />
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
       </main>
     </>
   )
