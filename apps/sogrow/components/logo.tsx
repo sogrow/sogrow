70d05c6d046f6893bd40cc3b2eb34ebbcb0a24ac
Alexander Schnebel <alexander@schnebel.dev>
2022-11-16 20:05:14 +0100

    feat: Add actual Logo to Sogrow

--- apps/sogrow/components/logo.tsx
+++ apps/sogrow/components/logo.tsx
@@ -1,15 +1,31 @@
-import { Bebas_Neue } from '@next/font/google';
-import Link from 'next/link';
+import { Bebas_Neue } from '@next/font/google'
+import Link from 'next/link'
+import Image from 'next/image'
+import logoPicture from '../public/images/logo.png'
 
 const bebasNeue = Bebas_Neue({ weight: '400', subsets: ['latin'] })
 
-export function Logo() {
+export interface LogoProps {
+  logoOnly?: boolean
+}
+
+export function Logo({ logoOnly = false }: LogoProps) {
+
+  if (logoOnly) {
+    return (
+      <Link href="/" className="flex items-center">
+        <Image className="mr-2" src={logoPicture} alt='Sogrow Logo' width={40} height={40} />
+      </Link>
+    )
+  }
+
   return (
-    <Link href="/" className={bebasNeue.className}>
-      <p className="text-3xl">
-      <span className="text-gray-500 text-bold">So</span>
-      <span className="text-gray-900">grow</span>
-    </p>
+    <Link href="/" className="flex items-center">
+      <Image className="mr-2" src={logoPicture} alt='Sogrow Logo' width={40} height={40} />
+      <p className={`text-3xl ${bebasNeue.className}`}>
+        <span className="text-bold text-gray-500">So</span>
+        <span className="text-gray-900">grow</span>
+      </p>
     </Link>
   )
 }
