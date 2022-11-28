26bdddb00aae29f82357f0086646897276388d8a
Alexander Schnebel <alexander@schnebel.dev>
2022-11-28 20:14:52 +0100

    ci: enable serverless deployment

---
+++ tools/github-actions/find-affected.js
@@ -0,0 +1,27 @@
+// @ts-check
+
+const { Workspaces } = require('@nrwl/devkit')
+const { execSync } = require('child_process')
+const { join } = require('path')
+
+const [tag = null] = process.argv.slice(2)
+
+if (!tag) {
+  console.error(`Missing Tag`)
+  process.exit(1)
+}
+
+const workspace = new Workspaces(join(__dirname, '../..')).readWorkspaceConfiguration()
+
+const projects = execSync(`npx nx print-affected --select projects`)
+  .toString('utf-8')
+  .trim()
+  .split(', ')
+  .filter((project) => !!project)
+
+const affected = projects.filter((project) => workspace.projects[project].tags?.includes(tag))
+
+const affectedMatrix = JSON.stringify(affected)
+const hasAffected = affected.length > 0
+
+console.log(JSON.stringify({ affectedMatrix, hasAffected }))
