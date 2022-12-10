43d28d66159c2115acc31cfc01a34ae863e1ac9b
Alexander Schnebel <alexander@schnebel.dev>
2022-12-10 19:21:39 +0100

    feat: reduce cold start for lambdas

--- apps/services/analytics/src/lambda.ts
+++ apps/services/analytics/src/lambda.ts
@@ -1,24 +1,35 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import serverlessExpress from '@vendia/serverless-express'
-import { Callback, Context, Handler } from 'aws-lambda'
+import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda'
 
 import { AppModule } from './app/app.module'
 import { Logger } from 'nestjs-pino'
 
 let server: Handler
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { bufferLogs: true })
-  app.useLogger(app.get(Logger))
-  app.useGlobalPipes(new ValidationPipe())
-  await app.init()
-
-  const expressApp = app.getHttpAdapter().getInstance()
-  return serverlessExpress({ app: expressApp })
+async function bootstrap(): Promise<Handler> {
+  const nestApp = await NestFactory.create(AppModule)
+  nestApp.useGlobalPipes(new ValidationPipe())
+  await nestApp.init()
+  nestApp.useLogger(nestApp.get(Logger))
+  const app = nestApp.getHttpAdapter().getInstance()
+  return serverlessExpress({ app })
 }
 
-export const handler: Handler = async (event: any, context: Context, callback: Callback) => {
-  server = server ?? (await bootstrap())
-  return server(event, context, callback)
+bootstrap().then((handler) => (server = handler))
+
+function waitForServer(event: APIGatewayEvent, context: Context, callback: Callback) {
+  setImmediate(() => {
+    if (!server) {
+      waitForServer(event, context, callback)
+    } else {
+      server(event, context, callback)
+    }
+  })
+}
+
+export const handler: Handler = (event: APIGatewayEvent, context: Context, callback: Callback) => {
+  if (server) return server(event, context, callback)
+  return waitForServer(event, context, callback)
 }
