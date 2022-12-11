f21cd2ba3493d01704a2ddc5d71d046deba906d7
Alexander Schnebel <alexander@schnebel.dev>
2022-12-11 19:04:46 +0100

    chore: increase timeout of lambda functions

--- serverless.base.ts
+++ serverless.base.ts
@@ -14,6 +14,7 @@ export const baseServerlessConfigProvider: Serverless['provider'] = {
     AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
   },
   region: env.region,
+  timeout: 10,
 }
 
 export const baseServerlessConfig: Partial<Serverless> = {
