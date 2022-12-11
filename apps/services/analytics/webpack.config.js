6e3709983742e1d1b0d72256209ce9349a838562
Alexander Schnebel <alexander@schnebel.dev>
2022-12-11 15:45:15 +0100

    fix(analytics): webpack.config

--- apps/services/analytics/webpack.config.js
+++ apps/services/analytics/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const slsw = require('serverless-webpack')
 const isLocal = slsw.lib.webpack.isLocal
 const nodeExternals = require('webpack-node-externals')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   mode: isLocal ? 'development' : 'production',
@@ -19,13 +20,10 @@ module.exports = {
     path: path.join(__dirname, '.webpack'),
     filename: '[name].js',
   },
+  externalsPresets: { node: true },
   externals: [
-    nodeExternals(),
     nodeExternals({
       modulesDir: path.resolve('../../../node_modules'),
-    }),
-    nodeExternals({
-      allowlist: [/^@sogrow\/services/],
     }),
   ],
   module: {
@@ -37,4 +35,14 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, '../../../libs/services/infra/gateway/src/lib/dal/prisma/schema.prisma'),
+          to: path.resolve(__dirname, '../../../apps/services/analytics/prisma/schema.prisma'),
+        },
+      ],
+    }),
+  ],
 }
