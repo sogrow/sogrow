b7e5c538082f9da990a49bae2a03d8bc3d140755
Alexander Schnebel <alexander@schnebel.dev>
2023-01-15 10:34:33 +0100

    feat: enhance User BOM

---
+++ libs/services/domain/bom/src/lib/user/User.ts
@@ -0,0 +1,20 @@
+import { UserPlan } from './UserPlan'
+import { UserRole } from './UserRole'
+
+export class User {
+  id: string
+  name: string
+  email: string
+  username: string
+  picture: string
+
+  completedOnboarding = false
+
+  userPlan: UserPlan
+  userRole: UserRole
+
+  locale?: string
+  country?: string
+  timeZone?: string
+  followersCount?: number
+}
--- libs/services/domain/bom/src/lib/user/index.ts
+++ libs/services/domain/bom/src/lib/user/index.ts
@@ -1,1 +1,3 @@
-export * from './user'
+export * from './User'
+export * from './UserPlan'
+export * from './UserRole'
