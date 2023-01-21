9dffc1f14030f1fc92597434d4902a6e0f18dcdb
Alexander Schnebel <alexander@schnebel.dev>
2023-01-21 20:28:24 +0100

    feat: remove unnecessary feature flags

--- apps/ui/sogrow/hooks/useFeatureToggle.ts
+++
@@ -1,8 +0,0 @@
-import { FeatureToggles } from '../config/featureToggle.type'
-import FeatureToggleContext from '../context/FeatureToggleContext'
-import React from 'react'
-
-export function useFeatureToggle(featureToggle: keyof FeatureToggles) {
-  const featureToggles = React.useContext(FeatureToggleContext)
-  return featureToggles[featureToggle]
-}
