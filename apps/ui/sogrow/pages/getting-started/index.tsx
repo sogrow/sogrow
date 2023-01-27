93ce908d6c81442ca3bfd754aee17e3e39b37245
Alexander Schnebel <alexander@schnebel.dev>
2023-01-27 20:39:24 +0100

    feat(ui): implement slot preference 1/2 draft1

--- apps/ui/sogrow/pages/getting-started/index.tsx
+++ apps/ui/sogrow/pages/getting-started/index.tsx
@@ -1,11 +1,47 @@
 import useTranslation from 'next-translate/useTranslation'
+import { Button } from 'flowbite-react'
 
 export function OnboardingPage() {
   const { t } = useTranslation('common')
 
+  const slots = [
+    {
+      id: 'auto',
+      title: t('slot_preference_auto_title'),
+      description: t('slot_preference_auto_description'),
+    },
+    {
+      id: 'manual',
+      title: t('slot_preference_manual_title'),
+      description: t('slot_preference_manual_description'),
+    },
+  ]
+
   return (
     <section className="pt-8">
-      <h1 className="font-grotesk text-xl">{t('slot_preference_headline')}</h1>
+      <h1 className="font-grotesk pb-6 text-xl">{t('slot_preference_headline')}</h1>
+      <ul className="mb-8 grid w-full gap-2 lg:grid-cols-2">
+        {slots.map((slot) => (
+          <li key={slot.id}>
+            <input type="radio" id={slot.id} name="slot-preference" value={slot.id} className="peer hidden" required />
+            <label
+              htmlFor={slot.id}
+              className="group inline-flex w-full cursor-pointer items-center justify-between rounded-lg border border-gray-200 bg-white p-5 text-zinc-900 hover:bg-violet-100 hover:text-gray-600 peer-checked:border-violet-600 peer-checked:bg-violet-50 peer-checked:text-violet-600"
+            >
+              <div className="block">
+                <div className="mb-2 w-full text-lg font-semibold">{slot.title}</div>
+                <div className="group-peer-checked:text-violet-600 w-full text-zinc-700">{slot.description}</div>
+              </div>
+            </label>
+          </li>
+        ))}
+      </ul>
+      <div className="flex items-center justify-between">
+        <span className="pl-4 text-sm text-zinc-600">1/2</span>
+        <Button color="purple" pill>
+          {t('slot_preference_button_next_label')}
+        </Button>
+      </div>
     </section>
   )
 }
--- apps/ui/sogrow/tailwind.config.js
+++ apps/ui/sogrow/tailwind.config.js
@@ -1,5 +1,6 @@
 const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind')
 const { join } = require('path')
+const plugin = require('tailwindcss/plugin')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -16,5 +17,23 @@ module.exports = {
       },
     },
   },
-  plugins: [require('@tailwindcss/forms'), require('flowbite/plugin'), require('tailwindcss-debug-screens')],
+  plugins: [
+    plugin(function groupPeer({ addVariant }) {
+      let pseudoVariants = [
+        // ... Any other pseudo variants you want to support.
+        // See https://github.com/tailwindlabs/tailwindcss/blob/6729524185b48c9e25af62fc2372911d66e7d1f0/src/corePlugins.js#L78
+        'checked',
+      ].map((variant) => (Array.isArray(variant) ? variant : [variant, `&:${variant}`]))
+
+      for (let [variantName, state] of pseudoVariants) {
+        addVariant(`group-peer-${variantName}`, (ctx) => {
+          let result = typeof state === 'function' ? state(ctx) : state
+          return result.replace(/&(\S+)/, ':merge(.peer)$1 ~ .group &')
+        })
+      }
+    }),
+    require('@tailwindcss/forms'),
+    require('flowbite/plugin'),
+    require('tailwindcss-debug-screens'),
+  ],
 }
