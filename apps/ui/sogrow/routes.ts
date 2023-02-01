1ade8a3090e6b64eba85ef2335526e9d9821f2c6
Alexander Schnebel <alexander@schnebel.dev>
2023-02-01 20:07:23 +0100

    feat(ui): enhance reactQuery abstraction

--- apps/ui/sogrow/routes.ts
+++ apps/ui/sogrow/routes.ts
@@ -2,4 +2,5 @@ const baseURL = 'https://api-preview.sogrow.cloud'
 
 export const apiRoutes = {
   getUserSettings: baseURL + '/identity/user-settings',
+  updateUserSettings: baseURL + '/identity/user-settings',
 }
--- apps/ui/sogrow/utils/reactQuery.ts
+++ apps/ui/sogrow/utils/reactQuery.ts
@@ -1,21 +1,80 @@
-import { QueryFunctionContext, useQuery, UseQueryOptions } from 'react-query'
-import axios from 'axios'
-import { getSession } from 'next-auth/react'
+import { QueryFunctionContext, useMutation, useQuery, useQueryClient, UseQueryOptions } from 'react-query'
+import { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios'
+import { api, RequestConfig } from './api'
+
+// See: https://www.smashingmagazine.com/2022/01/building-real-app-react-query/
 
 type QueryKeyT = [string, object | undefined]
 
-export const fetcher = async <T>({ queryKey, pageParam }: QueryFunctionContext<QueryKeyT>): Promise<T> => {
-  const session = await getSession()
-  if (session) {
-    axios.defaults.headers.common['Authorization'] = `Bearer ${session.accessToken}`
-  }
-  const [url, params] = queryKey
-  return axios.get<T>(url, { params: { ...params, pageParam } }).then((res) => res.data)
+export type UseFetchOptions = {
+  url: string | null
+  params?: object
+  requestConfig?: RequestConfig
+  axiosRequestConfig?: AxiosRequestConfig
+  useQueryOptions?: UseQueryOptions<any, Error, any, QueryKeyT>
 }
 
-export const useFetch = <T>(url: string | null, params?: object, config?: UseQueryOptions<T, Error, T, QueryKeyT>) => {
-  return useQuery<T, Error, T, QueryKeyT>([url, params], ({ queryKey }) => fetcher({ queryKey, meta: undefined }), {
-    enabled: !!url,
-    ...config,
+export type UseMutationOptions<T, S> = {
+  url: string
+  params?: object
+  requestConfig?: RequestConfig
+  axiosRequestConfig?: AxiosRequestConfig
+  updater?: ((oldData: T, newData: S) => T) | undefined
+}
+
+export const fetcher = async <T>(
+  { queryKey, pageParam }: QueryFunctionContext<QueryKeyT>,
+  requestConfig?: RequestConfig,
+  axiosRequestConfig?: AxiosRequestConfig,
+): Promise<T> => {
+  const [url, params] = queryKey
+  return api.get<T>(url, { ...params, ...pageParam }, requestConfig, axiosRequestConfig).then((res) => res.data)
+}
+
+export const useFetch = <T>({ url, params, requestConfig, axiosRequestConfig, useQueryOptions }: UseFetchOptions) => {
+  return useQuery<T, Error, T, QueryKeyT>(
+    [url, params],
+    ({ queryKey }) => fetcher({ queryKey, meta: undefined }, requestConfig, axiosRequestConfig),
+    {
+      enabled: !!url,
+      ...useQueryOptions,
+    },
+  )
+}
+
+const useGenericMutation = <T, S>(
+  func: (data: S) => Promise<AxiosResponse<S>>,
+  url: string,
+  params?: object,
+  updater?: ((oldData: T, newData: S) => T) | undefined,
+) => {
+  const queryClient = useQueryClient()
+
+  return useMutation<AxiosResponse, AxiosError, S>(func, {
+    onMutate: async (data) => {
+      await queryClient.cancelQueries([url, params])
+
+      const previousData = queryClient.getQueryData([url, params])
+
+      queryClient.setQueryData<T>([url, params], (oldData) => {
+        return updater ? updater(oldData, data as S) : (data as unknown as T)
+      })
+
+      return previousData
+    },
+    onError: (err, _, context) => {
+      queryClient.setQueryData([url, params], context)
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries([url, params])
+    },
   })
+}
+
+export const usePost = <T, S extends object>({ url, params, requestConfig, axiosRequestConfig, updater }: UseMutationOptions<T, S>) => {
+  return useGenericMutation<T, S>((data) => api.post<S>(url, data, requestConfig, axiosRequestConfig), url, params, updater)
+}
+
+export const useUpdate = <T, S extends object>({ url, params, requestConfig, axiosRequestConfig, updater }: UseMutationOptions<T, S>) => {
+  return useGenericMutation<T, S>((data) => api.put<S>(url, data, requestConfig, axiosRequestConfig), url, params, updater)
 }
